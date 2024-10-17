97f88d24f1a857f06c944c984bb2c5bd8c5942dc
sohail019 <shaikhhsohail0193@gmail.com>
2024-10-17 18:31:55 +0530

    Implemented Profile Page where i decoded token and return if user found

---
+++ 02_next_auth/src/app/api/users/profile/route.ts
@@ -0,0 +1,20 @@
+import User from "@/models/userModel";
+import { connect } from "@/database/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    //? Extract data from token
+    const userId = await getDataFromToken(request);
+
+    const user = User.findOne({_id: userId}).select("-password")
+    
+    //? Check if no user
+    return NextResponse.json({ message: "User Found", data: user });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
