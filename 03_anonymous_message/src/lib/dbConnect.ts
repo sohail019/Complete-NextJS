05a80e6f081a0faea0fb44625197eb1f067e5a9c
sohail019 <shaikhhsohail0193@gmail.com>
2024-10-21 13:18:22 +0530

    Created MongoDB Database Config with Mongoose

---
+++ 03_anonymous_message/src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect():Promise<void> {
+    if(connection.isConnected){
+        console.log("Already Connected to the Database")
+        return
+    }
+
+    try {
+        //* Attempt to connect database
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+        //? this sets the isConnected property of the connection object to the current connection state of the first database connection established by mongoose
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("Database Connected Successfully")
+    } catch (error) {
+        console.error("Database Connection Failed", error);
+
+        //! Gracefully exit in case of a connection error
+        process.exit(1)
+    }
+}
+
+export default dbConnect
