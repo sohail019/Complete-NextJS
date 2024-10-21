2f7ee5360bd781d1f6a3812eb930e83ed52996c6
sohail019 <shaikhhsohail0193@gmail.com>
2024-10-21 12:27:23 +0530

    Fixed Regex matches in email

--- 03_anonymous_message/src/model/User.ts
+++ 03_anonymous_message/src/model/User.ts
@@ -34,39 +34,40 @@ export interface User extends Document {
 //? Updated User Schema
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Username is Required"],
-        trim: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: [true, "Email is Required"],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, "Password is Required"],
-    },
-    verifyCode: {
-        type: String,
-        required: [true, "Verification Code is Required"]
-    },
-    verifyCodeExpiry: {
-        type: Date,
-        required: [true, "Verification Code Expiry is Required"]
-    },
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
-    isAcceptingMessages: {
-        type: Boolean,
-        default: true
-    },
-    messages: [MessageSchema]
-})
+  username: {
+    type: String,
+    required: [true, "Username is Required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is Required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is Required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verification Code is Required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verification Code Expiry is Required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
 
 
 //* This checks if a Mongoose model named "User" already exists! and if not, creates a new one using the UserSchema.
